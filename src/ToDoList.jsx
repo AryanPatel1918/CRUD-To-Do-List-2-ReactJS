7d860ef867a824cfb5fe8fdb0b8aaf0acd11d833
Aryan Patel <97249070+AryanPatel1918@users.noreply.github.com>
2025-04-18 12:48:15 -0400

    Update ToDoList.jsx
    Change 'To-Do-List' to 'To-Do List'

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -74,7 +74,7 @@ function ToDoList() {
 
     return (
         <div className='to-do-list'>
-            <h1>To-Do-List</h1>
+            <h1>To-Do List</h1>
             <div>
                 <input
                     className='add-task'
